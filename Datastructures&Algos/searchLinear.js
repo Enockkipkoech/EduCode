2486467326c3ad846c1ba095189918214746a2fa
Enockkipkoech <enockkipkoech19@gmail.com>
2023-03-08 16:36:02 +0300

    :rocket: Athentication and Authorization module:fire:

--- Datastructures&Algos/searchLinear.js
+++
@@ -1,9 +0,0 @@
-function linearSearch(arr, value) {
-	for (let i = 0; i <= arr.length; i++) {
-		if (arr[i] === value) return arr.indexOf(value);
-	}
-	return -1;
-}
-
-let arr = ["Black", "Best", "Goodness", 1, 1000];
-linearSearch(arr, 1000);
